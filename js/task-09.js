17258105e29a64e5fb846153a1f08e22ad587e12
Nataliia Khintsitska <Natalienumenna@gmail.com>
2022-01-20 00:11:54 +0200

    corrected task 9

--- js/task-09.js
+++ js/task-09.js
@@ -2,8 +2,8 @@ const btnColor = document.querySelector(".change-color");
 const spanColor = document.querySelector(".color");
 
 function getRandomHexColor() {
-    document.body.style.backgroundColor = #${Math.floor(Math.random() * 16777215).toString(16)};
-    spanColor.textContent = #${Math.floor(Math.random() * 16777215).toString(16)};
+    document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    spanColor.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 
